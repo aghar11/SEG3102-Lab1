9536ddd769cf8d0cc39cecddbbda54225841c4d8
Akarsh Gharge <akarshgharge20@gmail.com>
2023-09-13 14:17:38 -0400

    Created temp converter object

---
+++ src/app/temp-converter/temp-converter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temp-converter',
+  templateUrl: './temp-converter.component.html',
+  styleUrls: ['./temp-converter.component.css']
+})
+export class TempConverterComponent {
+
+}
