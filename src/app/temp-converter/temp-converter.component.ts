04dbb7f59301ecb7192c8d414049308c7a8d0129
Akarsh Gharge <akarshgharge20@gmail.com>
2023-09-14 14:22:25 -0400

    Set up HTML and TS for temp converter

--- src/app/temp-converter/temp-converter.component.ts
+++ src/app/temp-converter/temp-converter.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./temp-converter.component.css']
 })
 export class TempConverterComponent {
+  celsiusValue = 0;
+  fahrenheitValue = 0;
 
+  convertCelsius(value: string): void {
+    this.celsiusValue = Number(value);
+    this.fahrenheitValue = ((this.celsiusValue * 9) / 5) + 32; 
+  }
+
+  convertFahrenheit(value: string): void {
+    this.fahrenheitValue = Number(value);
+    this.celsiusValue = ((this.fahrenheitValue -32 ) * 5) / 9;
+  }
 }
